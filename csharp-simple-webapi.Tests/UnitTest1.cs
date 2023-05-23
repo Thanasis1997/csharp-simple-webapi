7e0802d512e627b5bb9e635e0efe5879cd2e56b1
uerbzr <nigel.sibbert@gmail.com>
2023-05-23 08:33:34 +0100

    ready

---
+++ csharp-simple-webapi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace csharp_simple_webapi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
